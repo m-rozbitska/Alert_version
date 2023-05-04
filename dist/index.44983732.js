1a9bb13a54ec5f8d5f4049423150a2421f1d7457
Mariia Rozbitska <melissa180lily@gmail.com>
2023-05-04 13:56:43 +0300

    feat: add js code to project

--- dist/index.44983732.js
+++ dist/index.44983732.js
@@ -1,2 +1,23 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const body = document.querySelector("body");
+    body.addEventListener("click", function(event) {
+        let target = event.target;
+        if (target.classList.contains("alert__close") == true) {
+            target.closest(".alert__item").remove();
+            if (!body.querySelector(".alert__item")) body.querySelector(".alert__wrapper").remove();
+        } else return;
+    });
+}); //function removeClass() {
+ //let body = document.querySelector('.body');
+ //body.addEventListener('click', function(event) {
+ //let target = event.target;
+ //if (target.className == ".alert__close") {
+ //target.closest('.alert__item').remove();
+ //} else {
+ //return;
+ //}
+ //});
+ //}
+ //document.addEventListener('DOMContentLoaded', removeClass);
 
 //# sourceMappingURL=index.44983732.js.map
